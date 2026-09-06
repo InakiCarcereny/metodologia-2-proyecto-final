e14e949ebb3e1a309c08a55036c61abd54b54b71
inaki carcereny <inakicarcereny.dev@gmail.com>
2026-09-06 13:08:31 -0300

    feat: update page for docker build test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import type { JSX } from 'react';
 
 export default function Home(): JSX.Element {
-  return <h1 className="font-bold text-2xl">Hello World!</h1>;
+  return <h1 className="p-6 font-bold text-2xl text-red-500">Hello World!</h1>;
 }
