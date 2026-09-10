920db8101d10bf3b84fdf151ac8d38d3b57d6508
inaki carcereny <inakicarcereny.dev@gmail.com>
2026-09-10 13:18:40 -0300

    feat: update biome config and return types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import type { Metadata } from 'next';
+import type { NextFontWithVariable } from 'next/dist/compiled/@next/font';
+import { Geist, Geist_Mono } from 'next/font/google';
+import type { JSX } from 'react';
 
-const geistSans = Geist({
+const geistSans: NextFontWithVariable = Geist({
   subsets: ['latin'],
   variable: '--font-geist-sans',
 });
 
-const geistMono = Geist_Mono({
+const geistMono: NextFontWithVariable = Geist_Mono({
   subsets: ['latin'],
   variable: '--font-geist-mono',
 });
@@ -17,7 +19,9 @@ export const metadata: Metadata = {
   title: 'Create Next App',
 };
 
-export default function RootLayout({ children }: LayoutProps<'/'>) {
+export default function RootLayout({
+  children,
+}: LayoutProps<'/'>): JSX.Element {
   return (
     <html
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
