815d7db0fe0ad830dbb98ed50a72beb30d77f795
ValentinDePascale <valentindp9@gmail.com>
2026-09-19 16:28:34 -0300

    feat: add post schema

---
+++ src/db/schema/post.ts
@@ -0,0 +1,9 @@
+import { pgTable, serial } from 'drizzle-orm/pg-core';
+import { users } from '../schema/user';
+
+export const user_posts = pgTable('user_posts', {
+  id: serial('id').primaryKey(),
+  idUser: serial('id_user').references(() => users.id, {
+    onDelete: 'set null',
+  }),
+});
