61289a3562e7919f2f168d4e38353f9cd887630a
ValentinDePascale <valentindp9@gmail.com>
2026-09-19 16:40:42 -0300

    refactor: export changed from post to userPost

--- src/db/schema/post.ts
+++ src/db/schema/post.ts
@@ -1,7 +1,7 @@
 import { pgTable, serial } from 'drizzle-orm/pg-core';
 import { users } from '../schema/user';
 
-export const user_posts = pgTable('user_posts', {
+export const userPosts = pgTable('user_posts', {
   id: serial('id').primaryKey(),
   idUser: serial('id_user').references(() => users.id, {
     onDelete: 'set null',
