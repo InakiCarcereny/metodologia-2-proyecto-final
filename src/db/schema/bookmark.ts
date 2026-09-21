6dfdfc1e69ab2b5e854d7f5ade3f8d965b5534a8
ValentinDePascale <valentindp9@gmail.com>
2026-09-21 17:35:01 -0300

    feat: add bookmark schema

---
+++ src/db/schema/bookmark.ts
@@ -0,0 +1,19 @@
+import { integer, pgTable, primaryKey, timestamp } from 'drizzle-orm/pg-core';
+import { posts } from './post';
+import { users } from './user';
+
+export const bookmarks = pgTable(
+  'bookmarks',
+  {
+    createdAt: timestamp('created_at', { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+    idPost: integer('id_post')
+      .notNull()
+      .references(() => posts.id, { onDelete: 'cascade' }),
+    idUser: integer('id_user')
+      .notNull()
+      .references(() => users.id, { onDelete: 'cascade' }),
+  },
+  (table) => [primaryKey({ columns: [table.idUser, table.idPost] })],
+);
