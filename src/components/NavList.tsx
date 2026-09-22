9456e46115dbfa1a3e137a2fb7c448d3687c9194
inaki carcereny <inakicarcereny.dev@gmail.com>
2026-09-22 14:46:24 -0300

    fix: adjust sidebar, nav list and user card styles

--- src/components/NavList.tsx
+++ src/components/NavList.tsx
@@ -29,8 +29,8 @@ const NAV_ITEMS: NavList = [
 
 export function NavList(): JSX.Element {
   return (
-    <nav className="h-full w-full">
-      <ul className="flex h-full w-full flex-col gap-1">
+    <nav className="w-full">
+      <ul className="flex w-full flex-col gap-1">
         {NAV_ITEMS.map((item) => (
           <NavItem key={item.path} label={item.label} path={item.path} />
         ))}
--- src/components/NavigationSideBar.tsx
+++ src/components/NavigationSideBar.tsx
@@ -4,26 +4,33 @@ import type { JSX } from 'react';
 import { LogoutButton } from '@/components/LogoutButton';
 import { NavList } from '@/components/NavList';
 import { Separator } from '@/components/Separator';
+import { TagList } from '@/components/TagList';
 import { UserCard } from '@/components/UserCard';
 
 export function NavigationSideBar(): JSX.Element {
   return (
-    <aside className="sticky top-0 flex flex-col gap-6 overflow-y-auto py-6">
-      <header>
-        <Link className="flex items-center gap-2" href="/home">
-          <GlobeIcon />
+    <aside className="sticky top-0 flex flex-col justify-between gap-6 overflow-y-auto py-6">
+      <div className="flex flex-col gap-4">
+        <header>
+          <Link className="flex items-center gap-2" href="/home">
+            <GlobeIcon />
 
-          <h1 className="font-bold text-lg">DevHub</h1>
-        </Link>
-      </header>
+            <h1 className="font-bold text-lg">DevHub</h1>
+          </Link>
+        </header>
 
-      <UserCard />
+        <UserCard />
 
-      <NavList />
+        <NavList />
 
-      <Separator />
+        <TagList />
+      </div>
 
-      <LogoutButton />
+      <div className="flex flex-col gap-2">
+        <Separator />
+
+        <LogoutButton />
+      </div>
     </aside>
   );
 }
