5e1cb08a42abbbe51635424e5c53fad79274bb27
ValentinDePascale <valentindp9@gmail.com>
2026-09-23 15:04:04 -0300

    feat: add github_account schema

---
+++ src/db/schema/github_account.ts
@@ -0,0 +1,14 @@
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { users } from './user';
+
+export const githubAccounts = pgTable('github_accounts', {
+  accessToken: text('access_token').notNull(),
+  githubId: integer('github_id').notNull().unique(),
+  githubUsername: text('github_username').notNull(),
+  idUser: integer('id_user')
+    .primaryKey()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  linkedAt: timestamp('linked_at', { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+});
